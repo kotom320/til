6e3dd08f0ef353bcd814e221381631e7b56ea142
dongwookko <100112762+dongwookko@users.noreply.github.com>
2025-05-29 14:39:44 +0900

    feat: MarkdownViewer에 rehype-highlight 추가 및 스타일 시트 수정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
 
 interface Props {
   content: string;
@@ -12,7 +13,10 @@ interface Props {
 export default function MarkdownViewer({ content }: Props) {
   return (
     <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl max-w-none dark:prose-invert">
-      <Markdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+      <Markdown
+        rehypePlugins={[rehypeRaw, rehypeHighlight]}
+        remarkPlugins={[remarkGfm]}
+      >
         {content}
       </Markdown>
     </div>
